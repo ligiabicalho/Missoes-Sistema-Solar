82b7b2a2bbcf0af9b59d1c39daab2d561694dd75
Lígia Bicalho <ligiabicalho@gmail.com>
2022-09-21 20:22:02 -0300

    Requisito 4

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Title from './Title';
+// import PropTypes from 'prop-types';
 
 class SolarSystem extends React.Component {
   render() {
+    const planets = 'Planetas';
     return (
-      <div data-testid="solar-system"> </div>
+      <div data-testid="solar-system">
+        <Title headline={ planets } />
+      </div>
     );
   }
 }

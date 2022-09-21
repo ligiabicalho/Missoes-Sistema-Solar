6709f583d8513006528249c1bcdfd43502de59dc
Lígia Bicalho <ligiabicalho@gmail.com>
2022-09-21 18:56:59 -0300

    Requisito 3 em andamento

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
+//  import SolarSystem from './components/SolarSystem';
+// import SolarSystem from './components/SolarSystem';
 
 class App extends React.Component {
   render() {
---
+++ src/components/Title.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Title extends React.Component {
+  render() {
+    const { headLine } = this.props;
+    return (
+      <h2>
+        {headLine}
+      </h2>
+    );
+  }
+}
+
+Title.propTypes = {
+  headLine: PropTypes.string.isRequired,
+};
+
+export default Title;

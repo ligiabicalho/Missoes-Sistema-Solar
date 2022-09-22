f9876c2af8d38714059de7fd842088c69b415775
Lígia Bicalho <ligiabicalho@gmail.com>
2022-09-22 08:51:22 -0300

    Requisito 6

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 class SolarSystem extends React.Component {
   render() {
-    const planets = 'Planetas';
+    // const { name, imagem } = planets;
     return (
       <div data-testid="solar-system">
-        <Title headline={ planets } />
+        <Title headline="Planetas" />
+        {planets.map((planet, i) => (
+          <PlanetCard
+            key={ i }
+            planetName={ planet.name }
+            planetImage={ planet.image }
+          />))}
       </div>
     );
   }

9829feba9654186aeecbe959878883aec0d72f8c
Lígia Bicalho <ligiabicalho@gmail.com>
2022-09-22 08:55:54 -0300

    Requisito 7

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
-//  import SolarSystem from './components/SolarSystem';
+import Missions from './components/Missions';
 // import SolarSystem from './components/SolarSystem';
 
 class App extends React.Component {
@@ -10,6 +10,7 @@ class App extends React.Component {
       <>
         <Header />
         <SolarSystem />
+        <Missions />
       </>
     );
   }

a59672d1377e27cff5aedc79a21b2cb7c49da9bd
Lígia Bicalho <ligiabicalho@gmail.com>
2023-03-30 10:35:51 -0300

    style

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ import Missions from './components/Missions';
 class App extends React.Component {
   render() {
     return (
-      <>
+      <div id="container">
         <Header />
         <SolarSystem />
         <Missions />
-      </>
+      </div>
     );
   }
 }
